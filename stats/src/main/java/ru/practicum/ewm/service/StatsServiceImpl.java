65b638894b370bb0133665e83e8610d7f1dc4525
Kirill Prokhorov <full-la@yandex.ru>
2023-04-12 21:46:38 +0300

    Еще попытка

---
+++ stats/src/main/java/ru/practicum/ewm/service/StatsServiceImpl.java
@@ -0,0 +1,41 @@
+package ru.practicum.ewm.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.ewm.dto.StatsClientDto;
+import ru.practicum.ewm.dto.StatsMapper;
+import ru.practicum.ewm.model.Stats;
+import ru.practicum.ewm.model.StatsClient;
+import ru.practicum.ewm.repository.StatsRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class StatsServiceImpl implements StatsService {
+
+    private final StatsRepository statsRepository;
+
+    @Transactional
+    @Override
+    public StatsClientDto saveStats(StatsClientDto statsClientDto) {
+        StatsClient statsClient = StatsMapper.toClass(statsClientDto);
+        statsClient.setTimestamp(LocalDateTime.now());
+        statsRepository.save(statsClient);
+        return StatsMapper.toDto(statsClient);
+    }
+
+    @Override
+    public List<Stats> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        if (unique) {
+            return statsRepository.getAllUniqueStats(start, end, uris, true);
+        } else {
+            return statsRepository.getAllStats(start, end, uris);
+        }
+    }
+
+}
