b3ec1494bf84f9a9297b1bf4d63567649473b2fa
Kirill Prokhorov <full-la@yandex.ru>
2023-04-14 18:05:27 +0300

    Добавил сортировку в запросе к бд

--- stats/src/main/java/ru/practicum/ewm/repository/StatsRepository.java
+++ stats/src/main/java/ru/practicum/ewm/repository/StatsRepository.java
@@ -12,14 +12,14 @@ import java.util.List;
 @Repository
 public interface StatsRepository extends JpaRepository<StatsClient, Long> {
 
-    @Query("SELECT new ru.practicum.ewm.model.Stats(s.app, s.uri, COUNT (s.ip)) " +
+    @Query("SELECT new ru.practicum.ewm.model.Stats(s.app, s.uri, COUNT (s.uri)) " +
             "FROM StatsClient s WHERE s.timestamp > ?1 AND s.timestamp < ?2 " +
-            "AND s.uri IN ?3 GROUP BY s.app, s.uri")
+            "AND s.uri IN ?3 GROUP BY s.app, s.uri ORDER BY COUNT (s.uri) DESC")
     List<Stats> getAllStats(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new ru.practicum.ewm.model.Stats(s.app, s.uri, COUNT (DISTINCT s.ip)) FROM " +
             "StatsClient s WHERE s.timestamp > ?1 AND s.timestamp < ?2 " +
-            "AND s.uri IN ?3 GROUP BY s.app, s.uri")
+            "AND s.uri IN ?3 GROUP BY s.app, s.uri ORDER BY COUNT (DISTINCT s.uri) DESC")
     List<Stats> getAllUniqueStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
 
 }
